965c919dced70bfebc64ac5b810072cbe922eddc
mhrnnoi <mehrannois20@gmail.com>
2022-09-01 20:52:41 +0430

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pow_max_min
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
